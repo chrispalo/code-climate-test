ab0a179f932383b8ee91bd1500a4b33b28f413a6
chrispalo <chrispalo@gmail.com>
2018-01-16 17:34:45 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+int main(int argc, char* arv[]) {
+  // Declare and new variable that we will 'forget' to delete.
+  int* x = new int[10];
+
+  std::cout << "Hello world!" << std::endl;
+
+  return 0;
+}
